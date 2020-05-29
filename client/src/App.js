63182da182c4c7ad5bdd10cf232d322d715ce906
monguis <jccnnetwork@gmail.com>
2020-05-29 11:16:31 -0600

    created early stage optionButtons

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
+import { Container,Row,Col } from "react-bootstrap"
 import logo from "./logo.svg";
 import "./App.css";
+import OptionButton from "./components/OptionButton";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <OptionButton></OptionButton>
+        </Container>
       </div>
     );
   }
 }
 
 export default App;
+
+{/* <div className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<h2>Welcome to React</h2>
+</div>
+<p className="App-intro">
+To get started, edit <code>src/App.js</code> and save to reload.
+</p> */}
---
+++ client/src/components/OptionButton/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assets/styles.css";
+
+const OptionButton = ()=>{
+    return (
+        <div>
+            <a className = "buttonSprite">
+faket
+            </a>
+        </div>
+    )
+}
+
+export default OptionButton;

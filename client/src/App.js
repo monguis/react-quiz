2c48e0d7b8c2be449f15e9401949031a081bff0a
monguis <jccnnetwork@gmail.com>
2020-05-29 13:52:37 -0600

    acomplished basic question display functionallity randomizing both questions and options order

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "./App.css";
 import Quiz from "./components/Quiz"
 
--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import OptionButton from "../OptionButton"
 import questions from "../questions.json"
 
 const Quiz = () => {
 
+    const [state, setState] = useState({
+        userAnswer: null,
+        currentQuestion: 0,
+        questionText: "",
+        options: []
+    })
 
-    const quess = questions;
-    console.log(quess)
-
+    const { currentQuestion, questionText, options } = state;
 
     const randomize = arr => {
         let newPos, temp;
-
         for (let i = arr.length - 1; i > 0; i--) {
             newPos = Math.floor(Math.random() * (i + 1));
             temp = arr[i];
@@ -21,29 +24,65 @@ const Quiz = () => {
         }
         return arr;
     }
-    
-    randomize(quess);
 
-    const test = quess[0];
+    useEffect(() => {
+        randomize(questions);
+    }, [])
+
+    useEffect(() => {
+        loadCurrentQuestion();
+    }, [currentQuestion])
+
+    const loadCurrentQuestion = () => {
+        setState({
+            ...state,
+            questionText: questions[state.currentQuestion].question,
+            options: randomize(questions[state.currentQuestion].options)
+        })
+    }
+    const nextQuestionHandler = () => {
+        setState({
+            ...state,
+            currentQuestion: currentQuestion + 1
+        })
+    }
 
     return (
-        <>
-            <Row>
-                <h3>{test.question}</h3>
-            </Row>
-            <Row>
-                <Col md={6} xs={12}>
-                    <OptionButton text={test.options[0]} />
-                    <OptionButton text={test.options[1]} />
-                </Col>
-                <Col md={6} xs={12}>
-                    <OptionButton text={test.options[2]} />
-                    <OptionButton text={test.options[3]} />
-                </Col>
-            </Row>
-            <Row><button>answer</button></Row>
-        </>
+        questionText ?
+            <>
+                <Row>
+                    <h3>{questionText}</h3>
+                </Row>
+                <Row>
+                    <Col md={6} xs={12}>
+                        <OptionButton text={options[0]} />
+                        <OptionButton text={options[1]} />
+                    </Col>
+                    <Col md={6} xs={12}>
+                        <OptionButton text={options[2]} />
+                        <OptionButton text={options[3]} />
+                    </Col>
+                </Row>
+                <Row><button onClick={nextQuestionHandler}>answer</button></Row>
+            </>
+            :
+            <h3>{"Connection Lost :'("}</h3>
     )
 }
 
 export default Quiz
+
+
+// localStorage.setItem("Todaytodo", JSON.stringify(daySchedule));
+
+// var storedSchedule = JSON.parse(localStorage.getItem("Todaytodo"));
+// if (storedSchedule) {
+//     if (storedSchedule.date !== currentdate.getDay() + "/" + currentdate.getMonth()) {
+//       localStorage.clear();
+//       localStorage.setItem("Todaytodo", JSON.stringify(daySchedule));
+//     } else {
+//       daySchedule = storedSchedule;
+//     }
+//   } else {
+//     localStorage.setItem("Todaytodo", JSON.stringify(daySchedule));
+//   }

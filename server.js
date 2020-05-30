5a69309e3eda01d5c17f0fef6d74a5f32e91f7be
monguis <jccnnetwork@gmail.com>
2020-05-30 11:06:52 -0600

    configured server and added basic interaction routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const questions = require("./models/questions.json");
+const answers = require("./models/answers.json");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,11 +12,24 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+
+app.use(express.json())
+
+app.get("/api/questions/", function (req, res) {
+  const questionsList = Object.values(questions);
+  res.send(questionsList);
+});
+
+app.post("/api/answer/", function (req, res) {
+  console.log("got in")
+  console.log(req.body)
+  res.send({ ...req.body, correct: true })
+});
+
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log("yasss");
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });

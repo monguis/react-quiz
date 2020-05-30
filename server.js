93d75dccb53877f7ffa6c31d3dce076c7f349499
monguis <jccnnetwork@gmail.com>
2020-05-30 11:37:44 -0600

    acomplished answer checker feature and questions delivery(with no answers)

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const questions = require("./models/questions.json");
-const answers = require("./models/answers.json");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -17,13 +16,19 @@ app.use(express.json())
 
 app.get("/api/questions/", function (req, res) {
   const questionsList = Object.values(questions);
-  res.send(questionsList);
+
+  const noAnswer = questionsList.map(question => { 
+    delete question.answer; 
+    return question 
+  });
+
+  res.send(noAnswer);
 });
 
 app.post("/api/answer/", function (req, res) {
-  console.log("got in")
-  console.log(req.body)
-  res.send({ ...req.body, correct: true })
+  const correctAnswer = questions.find(answer => answer.id === req.body.id).answer;
+
+  res.send({ ...req.body, correct: correctAnswer.toLowerCase === req.body.answer.toLowerCase });
 });
 
 app.get("*", function (req, res) {
